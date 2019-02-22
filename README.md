7a4bc3562aab0aa6808c794a7f6e3860a0e3510d
Thomas Amar <hello@ebabel.eu>
2019-02-22 21:26:01 +0100

    Basic chat setup.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # the-island
 The Island is a a prototype of a multi-player game socket.io server to simulate a small piece of land where each player can place a building that decays over time.
+
+## Install
+```
+npm install
+```
+
+## Run
+```
+npm start
+```
+
+Browse to http://localhost:3000
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+let chatLog = [];
+
+// Update the server chat log.
+// Emit a message to all connected players by default.
+const emitChatMessage = (m = [], sender = io) => {
+  chatLog = chatLog.concat(m);
+  m.map(msg => sender.emit('chat message', msg));
+};
+
+io.on('connection', (socket) => {
+  emitChatMessage(chatLog, socket); // Only send the history to newly connected player.
+  emitChatMessage(['Player connected.']);
+
+  socket.on('disconnect', () => {
+    emitChatMessage(['Player disconnected.']);
+  });
+
+  socket.on('chat message', (m) => {
+    emitChatMessage([m]);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`The Island server is listening on port ${port}.`);
+});
